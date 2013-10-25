b2574468716278e506ae80ba382a6fa2880054c9
cafrale <programador@outlook.com>
2013-10-25 11:29:19 -0200

    Adicionada função sonumeros

---
+++ sonumeros.php
@@ -0,0 +1,26 @@
+<?php
+
+function sonumeros($texto)
+  {
+     $atual = 0;
+     $numeros = '';
+     while ($atual <= strlen($texto))
+       {
+         $processando = substr($texto,$atual,1);
+         if ($processando == '0' or
+             $processando == '1' or
+             $processando == '2' or
+             $processando == '3' or
+             $processando == '4' or
+             $processando == '5' or
+             $processando == '6' or
+             $processando == '7' or
+             $processando == '8' or
+             $processando == '9')
+             { $numeros = $numeros . $processando; }
+         $atual++;
+       }
+    return $numeros;
+  }
+
+?>
